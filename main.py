aeabbd0a945de31144735ed170dea71ef5331b42
Vadim Vergasov <vadim.vergasov2003@gmail.com>
2020-06-26 01:38:38 +0300

    Added support for low-level role for invisible channel

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ class Bot(discord.Client):
         if message.author == self.user:
             return
 
-        if (
-            "@everyone" in [role.name for role in message.author.roles]
-            and len(message.author.roles) == 1
-        ):
+        if message.channel.name == "вход":
             if message.content.startswith("!nick"):
                 nickname = str(message.content).split(" ")[-1]
                 await message.channel.send(
@@ -28,6 +25,9 @@ class Bot(discord.Client):
                     )
                 roles = message.author.roles
                 roles.append(discord.utils.get(message.guild.roles, name="Участник"))
+                roles.remove(
+                    discord.utils.get(message.guild.roles, name="Новый участник")
+                )
                 try:
                     await message.author.edit(roles=roles)
                 except discord.errors.Forbidden:
@@ -40,9 +40,32 @@ class Bot(discord.Client):
                 await message.channel.send(
                     str(message.author.mention)
                     + ", напиши !nick Твой-ник-в-игре (например: !nick VadVergasov_EU). После этого тебе выдадут роль участника.",
-                    delete_after=300,
+                    delete_after=30,
                 )
             await message.delete()
+
+    async def on_member_join(self, member):
+        if member == self.user:
+            return
+
+        channel = discord.utils.get(member.guild.channels, name="вход")
+
+        roles = member.roles
+        roles.append(discord.utils.get(member.guild.roles, name="Новый участник"))
+        try:
+            await member.edit(roles=roles)
+        except discord.errors.Forbidden:
+            await channel.send(
+                str(member.mention)
+                + ", Не могу поменять твою роль, т.к. у меня не достаточно прав.",
+                delete_after=5,
+            )
+
+        await channel.send(
+            str(member.mention)
+            + ", напиши !nick Твой-ник-в-игре (например: !nick VadVergasov_EU). После этого тебе выдадут роль участника.",
+            delete_after=30,
+        )
 
 
 instance = Bot()
